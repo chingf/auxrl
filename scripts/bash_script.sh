0c0ba5f04b3d302c0f6a3442e778d80f25604c37
cf2794 <cf2794@axon.rc.zi.columbia.edu>
2023-02-01 13:33:03 -0500

    Remove unused transfer scripts and rename scripts

--- scripts/bash_script.sh
+++ scripts/bash_script.sh
@@ -3,28 +3,28 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-python bash_foraging_script.py 0 3 simplest_e32d64 6 0 &
-python bash_foraging_script.py 1 3 simplest_e32d64 6 1 &
-python bash_foraging_script.py 2 3 simplest_e32d64 6 2 &
+python run_foraging.py 0 3 simplest_e32d64 6 0 &
+python run_foraging.py 1 3 simplest_e32d64 6 1 &
+python run_foraging.py 2 3 simplest_e32d64 6 2 &
 
-python bash_foraging_script.py 0 3 simplest_e32d64 8 3 &
-python bash_foraging_script.py 1 3 simplest_e32d64 8 0 &
-python bash_foraging_script.py 2 3 simplest_e32d64 8 1 &
+python run_foraging.py 0 3 simplest_e32d64 8 3 &
+python run_foraging.py 1 3 simplest_e32d64 8 0 &
+python run_foraging.py 2 3 simplest_e32d64 8 1 &
 
-python bash_foraging_script.py 0 3 simplest_e32d64 10 2 &
-python bash_foraging_script.py 1 3 simplest_e32d64 10 3 &
-python bash_foraging_script.py 2 3 simplest_e32d64 10 0 &
+python run_foraging.py 0 3 simplest_e32d64 10 2 &
+python run_foraging.py 1 3 simplest_e32d64 10 3 &
+python run_foraging.py 2 3 simplest_e32d64 10 0 &
 
-python bash_foraging_script.py 0 3 simplest_e64d64 4 1 &
-python bash_foraging_script.py 1 3 simplest_e64d64 4 2 &
-python bash_foraging_script.py 2 3 simplest_e64d64 4 3 &
+python run_foraging.py 0 3 simplest_e64d64 4 1 &
+python run_foraging.py 1 3 simplest_e64d64 4 2 &
+python run_foraging.py 2 3 simplest_e64d64 4 3 &
 
-python bash_foraging_script.py 0 3 simplest_e64d64 6 0 &
-python bash_foraging_script.py 1 3 simplest_e64d64 6 1 &
-python bash_foraging_script.py 2 3 simplest_e64d64 6 2 &
+python run_foraging.py 0 3 simplest_e64d64 6 0 &
+python run_foraging.py 1 3 simplest_e64d64 6 1 &
+python run_foraging.py 2 3 simplest_e64d64 6 2 &
 
-python bash_foraging_script.py 0 3 simplest_e64d64 7 3 &
-python bash_foraging_script.py 1 3 simplest_e64d64 7 1 &
-python bash_foraging_script.py 2 3 simplest_e64d64 7 2 &
+python run_foraging.py 0 3 simplest_e64d64 7 3 &
+python run_foraging.py 1 3 simplest_e64d64 7 1 &
+python run_foraging.py 2 3 simplest_e64d64 7 2 &
 
 wait
