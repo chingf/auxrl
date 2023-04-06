7475a3ff7ff6bfc73226bc8f4b024312413ec8a9
cf2794 <cf2794@axon.rc.zi.columbia.edu>
2023-04-06 19:17:33 -0400

    Update notebooks

--- scripts/axon_script.sh
+++ scripts/axon_script.sh
@@ -3,9 +3,9 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-for JOB_IDX in {0..55}
+for JOB_IDX in {0..15}
 do
-    python run_foraging.py $JOB_IDX 56 dm 10 &
+    python run_figure8.py $JOB_IDX 16 dm 10 &
 done
 
 wait
