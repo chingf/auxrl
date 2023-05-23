5ed15713434060a7308848a1955d11fbc492e64b
Ching Fang <ching.fang@columbia.edu>
2023-05-23 20:28:28 +0000

    Save model parameters for scripts into a separate file

---
+++ scripts/model_parameters/gridworld.py
@@ -0,0 +1,154 @@
+def mf_grid():
+    fname_grid = ['mf_-1', 'mf_1', 'mf_2', 'mf_3']
+    loss_weights_grid = [
+        [0,0,0,1E-1],
+        [0,0,0,1E1],
+        [0,0,0,1E2],
+        [0,0,0,1E3],
+        ]
+    param_updates = [{}, {}, {}, {}]
+    return fname_grid, loss_weights_grid, param_updates
+
+
+def selected_models():
+    pass
+
+def full_grid():
+    fname_grid = [
+        'g0_-2_entro1_noMF',
+        'g0.8_-4_entro1_noMF',
+    
+        'g0_-1',
+        'g0_-2',
+        'g0_-3',
+    
+        'g0.25_-1',
+        'g0.25_-2',
+        'g0.25_-3',
+    
+        'g0.5_-1',
+        'g0.5_-2',
+        'g0.5_-3',
+    
+        'g0.8_-1',
+        'g0.8_-2',
+        'g0.8_-3',
+    
+        'mf',
+        'entro_0',
+        'entro_1',
+        'entro_2',
+        'entro_3',
+    
+        'g0_-2_entro0',
+        'g0_-2_entro1',
+        'g0_-2_entro2',
+        'g0_-2_entro3',
+    
+        'g0.25_-3_entro0',
+        'g0.25_-3_entro1',
+        'g0.25_-3_entro2',
+        'g0.25_-3_entro3',
+    
+        'g0.5_-3_entro0',
+        'g0.5_-3_entro1',
+        'g0.5_-3_entro2',
+        'g0.5_-3_entro3',
+    
+        'g0.8_-4_entro0',
+        'g0.8_-4_entro1',
+        'g0.8_-4_entro2',
+        'g0.8_-4_entro3',
+        ]
+    
+    loss_weights_grid = [
+        [1E-2, 1E1, 1E1, 0],
+        [1E-4, 1E1, 1E1, 0],
+    
+        [1E-1, 0, 0, 1],
+        [1E-2, 0, 0, 1],
+        [1E-3, 0, 0, 1],
+    
+        [1E-1, 0, 0, 1],
+        [1E-2, 0, 0, 1],
+        [1E-3, 0, 0, 1],
+    
+        [1E-1, 0, 0, 1],
+        [1E-2, 0, 0, 1],
+        [1E-3, 0, 0, 1],
+    
+        [1E-1, 0, 0, 1],
+        [1E-2, 0, 0, 1],
+        [1E-3, 0, 0, 1],
+    
+        [0, 0, 0, 1],
+        [0, 1E0, 1E0, 1],
+        [0, 1E1, 1E1, 1],
+        [0, 1E2, 1E2, 1],
+        [0, 1E3, 1E3, 1],
+    
+        [1E-2, 1E0, 1E0, 1],
+        [1E-2, 1E1, 1E1, 1],
+        [1E-2, 1E2, 1E2, 1],
+        [1E-2, 1E3, 1E3, 1],
+    
+        [1E-3, 1E0, 1E0, 1],
+        [1E-3, 1E1, 1E1, 1],
+        [1E-3, 1E2, 1E2, 1],
+        [1E-3, 1E3, 1E3, 1],
+    
+        [1E-3, 1E0, 1E0, 1],
+        [1E-3, 1E1, 1E1, 1],
+        [1E-3, 1E2, 1E2, 1],
+        [1E-3, 1E3, 1E3, 1],
+    
+        [1E-4, 1E0, 1E0, 1],
+        [1E-4, 1E1, 1E1, 1],
+        [1E-4, 1E2, 1E2, 1],
+        [1E-4, 1E3, 1E3, 1],
+        ]
+    
+    param_updates = [
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.8}},
+    
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+    
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.25}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.25}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.25}},
+    
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.5}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.5}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.5}},
+    
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.8}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.8}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.8}},
+    
+        {}, {}, {}, {}, {},
+    
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+    
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.25}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.25}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.25}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.25}},
+    
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.5}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.5}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.5}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.5}},
+    
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.8}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.8}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.8}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.8}},
+        ]
+
+    return fname_grid, loss_weights_grid, param_updates
