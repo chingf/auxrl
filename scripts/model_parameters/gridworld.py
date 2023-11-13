8d81a67dc79c4e7bfc263aa7a9536ca7b418fda4
cf2794 <cf2794@axon.rc.zi.columbia.edu>
2023-11-13 18:20:34 -0500

    Update model parameters

--- scripts/model_parameters/gridworld.py
+++ scripts/model_parameters/gridworld.py
@@ -162,6 +162,69 @@ def test_full():
         ]
     return fname_grid, loss_weights_grid, param_updates
 
+def test_full_mf_g0_only():
+    fname_grid = [
+        'mf-1',
+        'mf0',
+        'mf1',
+        'mf2',
+
+        'g0_-3_entro-1',
+        'g0_-3_entro-2',
+        'g0_-3_entro0',
+        'g0_-3_entro1',
+
+        'g0_-2_entro-1',
+        'g0_-2_entro-2',
+        'g0_-2_entro0',
+        'g0_-2_entro1',
+
+        'g0_-1_entro-1',
+        'g0_-1_entro-2',
+        'g0_-1_entro0',
+        'g0_-1_entro1',
+        ]
+    loss_weights_grid = [
+        [0, 0, 0, 1E-1],
+        [0, 0, 0, 1E0],
+        [0, 0, 0, 1E1],
+        [0, 0, 0, 1E2],
+
+        [1E-3, 1E-1, 1E-1, 1],
+        [1E-3, 1E-2, 1E-2, 1],
+        [1E-3, 1E0, 1E0, 1],
+        [1E-3, 1E1, 1E1, 1],
+
+        [1E-2, 1E-1, 1E-1, 1],
+        [1E-2, 1E-2, 1E-2, 1],
+        [1E-2, 1E0, 1E0, 1],
+        [1E-2, 1E1, 1E1, 1],
+
+        [1E-1, 1E-1, 1E-1, 1],
+        [1E-1, 1E-2, 1E-2, 1],
+        [1E-1, 1E0, 1E0, 1],
+        [1E-1, 1E1, 1E1, 1],
+        ]
+    param_updates = [
+        {}, {}, {}, {},
+
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        {'agent_args': {'pred_TD': True, 'pred_len': 2, 'pred_gamma': 0.}},
+        ]
+    return fname_grid, loss_weights_grid, param_updates
+
 def mf_grid():
     fname_grid = ['mf0', 'mf-1', 'mf1', 'mf2']
     loss_weights_grid = [
